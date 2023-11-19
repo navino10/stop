5affe27e4f76fe63dbdd373d9b2cc209800fdfe6
navino10 <132859693+navino10@users.noreply.github.com>
2023-11-19 11:46:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+document.getElementById('signupBtn').addEventListener('click', function() {
+    document.getElementById('signupModal').style.display = 'block';
+});
+
+document.getElementById('closeSignInModal').addEventListener('click', function() {
+    document.getElementById('signInModal').style.display = 'none';
+});
+
+
+document.getElementById('signinBtn').addEventListener('click', function() {
+    document.getElementById('signinModal').style.display = 'block';
+});
+
+document.getElementById('closeSignInModal').addEventListener('click', function() {
+    document.getElementById('signInModal').style.display = 'none';
+});
